febd9da4fb60f6327220401557629e203720f9a4
Joe Chu <joechu1983@gmail.com>
2019-02-27 07:26:58 +0000

    Added some report download and fixed some things

--- app/controllers/scores.rb
+++ app/controllers/scores.rb
@@ -8,5 +8,23 @@ module Judy
       status 200
       erb :'scores/index', :locals => { :abstracts => @abstracts, :sort => @sort, :user => session[:user] }
     end
+
+    get '/scores/csv/aggregate?' do
+      @sort = params[:sort] || 'mean'
+      @abstracts = Abstract.fetch_all_abstracts_and_scores(:sort => @sort)
+      content_type 'application/csv'
+      attachment "Accelerate CFP Review - Aggregated Scores.csv"
+      status 200
+      erb :'scores/csv-aggregate', :layout => false, :locals => { :abstracts => @abstracts, :sort => @sort, :user => session[:user] }
+    end
+
+    get '/scores/csv/individual?' do
+      @sort = params[:sort] || 'mean'
+      @abstracts = Abstract.fetch_all_abstracts_and_scores(:sort => @sort)
+      content_type 'application/csv'
+      attachment "Accelerate CFP Review - Individual Scores and Comments.csv"
+      status 200
+      erb :'scores/csv-individual', :layout => false, :locals => { :abstracts => @abstracts, :sort => @sort, :user => session[:user] }
+    end
   end
 end
